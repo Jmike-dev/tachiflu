e7955bbd5c38e3eef1862162f579961acd481761
Michael Gichuhi <gmikekamau@gmail.com>
2024-03-26 22:22:59 +0300

    ✨ added routing

--- tachiflu/lib/main.dart
+++ tachiflu/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tachiflu/routes/route_config.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,15 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('hello tachiflu'),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: definedRoutes,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
